b98b28e08b8df302889582f9d116aa0ba315e635
Karan323264 <karankkchintu@gmail.com>
2026-06-05 16:38:35 +0530

    spring cicd first update

---
+++ src/main/java/com/bound/cicd/SpringCicdPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.bound.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCicdPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCicdPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bound/cicd/SpringCicdPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bound.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCicdPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
